db574d6045c4bf7c7c349dc98d1adde456b15f09
elapage <62256007+elapage@users.noreply.github.com>
2024-11-08 15:37:18 -0600

    Point Exercise uploaded
    Driver for the Point Exercise uploaded

---
+++ 03_Hierarchies/PointExercise/PointDriver.java
@@ -0,0 +1,33 @@
+/**
+ * A driver class to test the Point2D hierarchy. Please complete and
+ * use!
+ */
+
+public class PointDriver
+{
+    public static void main()
+    {
+        final int MAX = 40; //maximum number of test cases
+        for(int i = 0; i<MAX; i++)
+        {
+            Point2D point;
+            int choice; //choice is a random number between 1 and 2 inclusive.
+
+            //choose a 2D point
+            if(choice == 1)
+            {
+                //initialize point as a 2D point at (0, 0)
+            }
+            else if(choice == 2)
+            {
+                //initialize point as a 3D point at (0, 0, 0)
+            }
+
+            //print the point.
+
+            //initialize point as a 3D point with random (x, y, z), each value between -100 and 100
+
+            //print the point
+        }
+    }
+}
